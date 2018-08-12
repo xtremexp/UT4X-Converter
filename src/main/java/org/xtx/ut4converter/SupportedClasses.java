a49e786974a0d44db354573519bb922a55e37043
xtremexp <unrealxtx@gmail.com>
2018-08-12 19:53:52 +0200

    refactor naming

--- src/main/java/org/xtx/ut4converter/SupportedClasses.java
+++ src/main/java/org/xtx/ut4converter/SupportedClasses.java
@@ -58,7 +58,7 @@ public class SupportedClasses {
 	 * @param classNames
 	 * @param utActorClass
 	 */
-	private void putUtClass(Class<? extends T3DActor> utActorClass, String... classNames) {
+	private void registerUClass(Class<? extends T3DActor> utActorClass, String... classNames) {
 		if (classNames == null) {
 			return;
 		}
@@ -102,7 +102,7 @@ public class SupportedClasses {
 		HashMap<String, T3DMatch.Match> hm = mc.getActorClassMatch();
 
 		for (String c : hm.keySet()) {
-			putUtClass(hm.get(c).t3dClass, c);
+			registerUClass(hm.get(c).t3dClass, c);
 		}
 	}
 
@@ -125,41 +125,41 @@ public class SupportedClasses {
 	private void initialize() {
 
 		// TODO move zones to match with T3DZoneInfo.class
-		putUtClass(T3DBrush.class, "Brush", "LavaZone", "WaterZone", "SlimeZone", "NitrogenZone", "PressureZone", "VacuumZone", "BlockAll");
+		registerUClass(T3DBrush.class, "Brush", "LavaZone", "WaterZone", "SlimeZone", "NitrogenZone", "PressureZone", "VacuumZone", "BlockAll");
 
         for (T3DBrush.BrushClass brushClass : T3DBrush.BrushClass.values()) {
 
             // mover is special case because is dependant of staticmesh for UE2
             // not brush (UE1)
             if (brushClass != T3DBrush.BrushClass.Mover) {
-                putUtClass(T3DBrush.class, brushClass.name());
+                registerUClass(T3DBrush.class, brushClass.name());
             }
         }
 
-		putUtClass(mapConverter.isFrom(UnrealEngine.UE1) ? T3DMover.class : T3DMoverSM.class, "Mover", "AttachMover", "AssertMover", "RotatingMover", "ElevatorMover", "MixMover", "GradualMover",
+		registerUClass(mapConverter.isFrom(UnrealEngine.UE1) ? T3DMover.class : T3DMoverSM.class, "Mover", "AttachMover", "AssertMover", "RotatingMover", "ElevatorMover", "MixMover", "GradualMover",
 				"LoopMover", "InterpActor");
 
-		//putUtClass()
-		putUtClass(T3DPlayerStart.class, "PlayerStart", "UTTeamPlayerStart", "UTWarfarePlayerStart");
-		putUtClass(T3DStaticMesh.class, "StaticMeshActor");
+		//registerUClass()
+		registerUClass(T3DPlayerStart.class, "PlayerStart", "UTTeamPlayerStart", "UTWarfarePlayerStart");
+		registerUClass(T3DStaticMesh.class, "StaticMeshActor");
 		// TODO args param only
 		final boolean useUbClasses = true;
 
 		// SPECIFIC Unreal 1 conversion test
 		if(mapConverter.isFrom(UnrealEngine.UE1) && useUbClasses) {
-			putUtClass(T3DDispatcher.class, "Dispatcher");
-			putUtClass(T3DSpecialEvent.class, "SpecialEvent");
-			putUtClass(T3DMusicEvent.class, "MusicEvent");
-			putUtClass(T3DTranslatorEvent.class, "TranslatorEvent");
-			putUtClass(T3DCreatureFactory.class, "CreatureFactory");
-			putUtClass(T3DSpawnPoint.class, "SpawnPoint");
-			putUtClass(T3DAlarmPoint.class, "AlarmPoint");
-			putUtClass(T3DCounter.class, "Counter");
+			registerUClass(T3DDispatcher.class, "Dispatcher");
+			registerUClass(T3DSpecialEvent.class, "SpecialEvent");
+			registerUClass(T3DMusicEvent.class, "MusicEvent");
+			registerUClass(T3DTranslatorEvent.class, "TranslatorEvent");
+			registerUClass(T3DCreatureFactory.class, "CreatureFactory");
+			registerUClass(T3DSpawnPoint.class, "SpawnPoint");
+			registerUClass(T3DAlarmPoint.class, "AlarmPoint");
+			registerUClass(T3DCounter.class, "Counter");
 
-			putUtClass(T3DUE1Trigger.class, "Trigger", "TeamTrigger", "ZoneTrigger", "TimedTrigger", "Trigger_ASTeam", "ScriptedTrigger", "VolumeTrigger", "MessageTrigger", "CrowdTrigger", "UseTrigger", "MusicTrigger", "RedirectionTrigger",
+			registerUClass(T3DUE1Trigger.class, "Trigger", "TeamTrigger", "ZoneTrigger", "TimedTrigger", "Trigger_ASTeam", "ScriptedTrigger", "VolumeTrigger", "MessageTrigger", "CrowdTrigger", "UseTrigger", "MusicTrigger", "RedirectionTrigger",
 					"GravityTrigger", "MaterialTrigger", "TriggeredCondition");
 		} else {
-			putUtClass(T3DTrigger.class, "Trigger", "TeamTrigger", "ZoneTrigger", "TimedTrigger", "Trigger_ASTeam", "ScriptedTrigger", "VolumeTrigger", "MessageTrigger", "CrowdTrigger", "UseTrigger", "MusicTrigger", "RedirectionTrigger",
+			registerUClass(T3DTrigger.class, "Trigger", "TeamTrigger", "ZoneTrigger", "TimedTrigger", "Trigger_ASTeam", "ScriptedTrigger", "VolumeTrigger", "MessageTrigger", "CrowdTrigger", "UseTrigger", "MusicTrigger", "RedirectionTrigger",
 					"GravityTrigger", "MaterialTrigger", "TriggeredCondition");
 		}
 
@@ -169,64 +169,64 @@ public class SupportedClasses {
 			for (T3DLight.UE12_LightActors ut99LightActor : T3DLight.UE12_LightActors.values()) {
 
 				if(useUbClasses && ut99LightActor == T3DLight.UE12_LightActors.TriggerLight){
-					putUtClass(T3DTriggerLight.class, ut99LightActor.name());
+					registerUClass(T3DTriggerLight.class, ut99LightActor.name());
 				} else {
-					putUtClass(T3DLight.class, ut99LightActor.name());
+					registerUClass(T3DLight.class, ut99LightActor.name());
 				}
 			}
 		} else if (mapConverter.isFrom(UnrealEngine.UE3)) {
 
-			putUtClass(T3DNote.class, "Note");
+			registerUClass(T3DNote.class, "Note");
 
 			for (T3DSound.UE3_AmbientSoundActor ue3SoundActor : T3DSound.UE3_AmbientSoundActor.values()) {
-				putUtClass(T3DSound.class, ue3SoundActor.name());
+				registerUClass(T3DSound.class, ue3SoundActor.name());
 			}
 
 			for (T3DLight.UE3_LightActor ue4LightActor : T3DLight.UE3_LightActor.values()) {
-				putUtClass(T3DLight.class, ue4LightActor.name());
+				registerUClass(T3DLight.class, ue4LightActor.name());
 			}
 		}
 
-		putUtClass(T3DLevelInfo.class, "LevelInfo");
+		registerUClass(T3DLevelInfo.class, "LevelInfo");
 
 		if (mapConverter.isTo(UTGames.UnrealEngine.UE3, UTGames.UnrealEngine.UE4)) {
 			// disabled until working good
-			putUtClass(T3DZoneInfo.class, "ZoneInfo");
+			registerUClass(T3DZoneInfo.class, "ZoneInfo");
 		}
 
 		// terrain conversion disabled until working good
 		if (mapConverter.isFrom(UTGames.UnrealEngine.UE2)) {
-			putUtClass(T3DUE2Terrain.class, "TerrainInfo");
+			registerUClass(T3DUE2Terrain.class, "TerrainInfo");
 		}
 
 		for (T3DLight.UE4_LightActor ue34LightActor : T3DLight.UE4_LightActor.values()) {
-			putUtClass(T3DLight.class, ue34LightActor.name());
+			registerUClass(T3DLight.class, ue34LightActor.name());
 		}
 
 		// UT99 Assault
-		putUtClass(T3DASObjective.class, "FortStandard");
-		putUtClass(T3DASCinematicCamera.class, "SpectatorCam");
-		putUtClass(T3DASInfo.class, "AssaultInfo");
+		registerUClass(T3DASObjective.class, "FortStandard");
+		registerUClass(T3DASCinematicCamera.class, "SpectatorCam");
+		registerUClass(T3DASInfo.class, "AssaultInfo");
 
 		// UT2004, UT99 Dom
-		putUtClass(DomPoint.class, "xDomPointA", "xDomPointB", "ControlPoint");
+		registerUClass(DomPoint.class, "xDomPointA", "xDomPointB", "ControlPoint");
 		
-		putUtClass(DefensePoint.class, "UTDefensePoint");
+		registerUClass(DefensePoint.class, "UTDefensePoint");
 
 		// UT3
-		putUtClass(DecalActor.class, "DecalActor");
-		putUtClass(HeightFog.class, "HeightFog");
+		registerUClass(DecalActor.class, "DecalActor");
+		registerUClass(HeightFog.class, "HeightFog");
 
-		putUtClass(T3DTeleporter.class, "Teleporter", "FavoritesTeleporter", "VisibleTeleporter", "UTTeleporter", "UTTeleporterCustomMesh");
+		registerUClass(T3DTeleporter.class, "Teleporter", "FavoritesTeleporter", "VisibleTeleporter", "UTTeleporter", "UTTeleporterCustomMesh");
 
 		if(useUbClasses) {
-			putUtClass(T3DSound.class, "AmbientSound", "AmbientSoundSimple");
-			putUtClass(T3DDynamicAmbientSound.class, "DynamicAmbientSound");
+			registerUClass(T3DSound.class, "AmbientSound", "AmbientSoundSimple");
+			registerUClass(T3DDynamicAmbientSound.class, "DynamicAmbientSound");
 		} else {
-			putUtClass(T3DSound.class, "AmbientSound", "DynamicAmbientSound", "AmbientSoundSimple");
+			registerUClass(T3DSound.class, "AmbientSound", "DynamicAmbientSound", "AmbientSoundSimple");
 		}
-		putUtClass(T3DLiftExit.class, "LiftExit", "UTJumpLiftExit");
-		putUtClass(T3DJumpPad.class, "Kicker", "Jumper", "BaseJumpPad_C", "U2Kicker", "U2KickReflector", "xKicker", "UTJumppad");
+		registerUClass(T3DLiftExit.class, "LiftExit", "UTJumpLiftExit");
+		registerUClass(T3DJumpPad.class, "Kicker", "Jumper", "BaseJumpPad_C", "U2Kicker", "U2KickReflector", "xKicker", "UTJumppad");
 
 		addMatches(mapConverter);
 
@@ -245,7 +245,7 @@ public class SupportedClasses {
 		uneededActors.add("ProscribedReachSpec"); // ut3
 
 		if (USE_CUSTOM) {
-			putUtClass(SpecialEvent.class, "SpecialEvent");
+			registerUClass(SpecialEvent.class, "SpecialEvent");
 		}
 	}
 
