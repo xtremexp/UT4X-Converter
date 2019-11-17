aaae80525e64359a2b11286016747ec75b826c00
xtremexp <unrealxtx@gmail.com>
2019-11-17 10:42:32 +0100

    refactor: move to uiutils

--- src/main/java/org/xtx/ut4converter/ui/MainSceneController.java
+++ src/main/java/org/xtx/ut4converter/ui/MainSceneController.java
@@ -38,6 +38,8 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static org.xtx.ut4converter.tools.UIUtils.openUrl;
+
 /**
  * FXML Controller class TODO i18n
  * 
@@ -195,65 +197,7 @@ public class MainSceneController implements Initializable {
 		}
 	}
 
-	/**
-	 * Opens url in web browser
-	 *
-	 * @param url
-	 *            Url to open with web browser
-	 */
-	public static void openUrl(String url, boolean confirmBeforeOpen, String message) {
 
-		if (url == null) {
-			return;
-		}
-
-		if (confirmBeforeOpen) {
-			Alert alert = new Alert(AlertType.CONFIRMATION);
-			alert.setTitle("Confirmation");
-			alert.setHeaderText("Web browser access");
-
-			message = message != null ? (message + " \n") : "";
-			message += "Do you want to open web browser to this url ?\n" + url;
-
-			alert.setContentText(message);
-
-			Optional<ButtonType> result = alert.showAndWait();
-
-			if (result.get() != ButtonType.OK) {
-				return;
-			}
-		} else if (message != null) {
-
-			Alert alert = new Alert(AlertType.CONFIRMATION);
-			alert.setTitle("Information");
-			alert.setHeaderText("Message");
-
-			alert.setContentText(message);
-
-			Optional<ButtonType> result = alert.showAndWait();
-
-			if (result.get() != ButtonType.OK) {
-				return;
-			}
-		}
-
-		if (Desktop.isDesktopSupported()) {
-			try {
-				Desktop desktop = Desktop.getDesktop();
-
-				desktop.browse(new URI(url));
-			} catch (URISyntaxException | IOException ex) {
-				Logger.getLogger(MainSceneController.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		} else {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("Error");
-			alert.setHeaderText("Impossible to open web browser");
-			alert.setContentText("Your system is not or does not support desktop. \n Manually go to:" + url);
-
-			alert.showAndWait();
-		}
-	}
 
 	/**
 	 * OPen web browser to ut4 converter topic at ut forums
