53be7d01bf8dd8a2c9ecdd79f9d54d65b80bb5f9
xtremexp <unrealxtx@gmail.com>
2019-12-08 15:38:49 +0100

    fix(texture): fixed no textures exported with newer umodel program

--- src/main/java/org/xtx/ut4converter/MainApp.java
+++ src/main/java/org/xtx/ut4converter/MainApp.java
@@ -28,7 +28,7 @@ public class MainApp extends Application {
 	/**
 	 * Version of UT4 Converter
 	 */
-	public static final String VERSION = "0.9.1";
+	public static final String VERSION = "0.9.2";
 
 	/**
 	 * Author
