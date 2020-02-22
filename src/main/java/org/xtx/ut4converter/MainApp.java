86d1ca712e1f1c3bb3a4f3ed8efc1e7b5670afa1
xtremexp <unrealxtx@gmail.com>
2020-02-22 12:40:30 +0100

    ui: update version to 1.0.0

--- src/main/java/org/xtx/ut4converter/MainApp.java
+++ src/main/java/org/xtx/ut4converter/MainApp.java
@@ -32,7 +32,7 @@ public class MainApp extends Application {
 	/**
 	 * Version of UT4 Converter
 	 */
-	public static final String VERSION = "0.9.5";
+	public static final String VERSION = "1.0.0";
 
 	/**
 	 * Author
