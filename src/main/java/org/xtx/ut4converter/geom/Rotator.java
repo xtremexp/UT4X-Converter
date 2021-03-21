5f20f961f4dde247dd9c9b6bea48be119a10a4f7
xtremexp <unrealxtx@gmail.com>
2021-03-21 09:32:06 +0100

    refactor(geometry): added some utils classes

--- src/main/java/org/xtx/ut4converter/geom/Rotator.java
+++ src/main/java/org/xtx/ut4converter/geom/Rotator.java
@@ -8,12 +8,19 @@ package org.xtx.ut4converter.geom;
 import org.xtx.ut4converter.UTGames.UnrealEngine;
 
 import javax.vecmath.Vector3d;
+import java.util.Objects;
 
 /**
  *
  * @author XtremeXp
  */
 public class Rotator {
+
+	/**
+	 * If true range of pitch yaw, roll is [-360;+360]
+	 * else [-32768:+32768]
+	 */
+	boolean isUE4Space;
 
 	/**
 	 * Rotate using X axis
@@ -28,6 +35,12 @@ public class Rotator {
 	 */
 	double roll;
 
+	/**
+	 * Creates a rotator in UE4 range
+	 * @param pitch
+	 * @param yaw
+	 * @param roll
+	 */
 	public Rotator(double pitch, double yaw, double roll) {
 		this.pitch = pitch;
 		this.yaw = yaw;
@@ -39,12 +52,92 @@ public class Rotator {
 		this.yaw = rotator.z;
 		this.roll = rotator.x;
 	}
-	
+
+	public double getPitch() {
+		return pitch;
+	}
+
+	public void setPitch(double pitch) {
+		this.pitch = pitch;
+	}
+
+	public double getYaw() {
+		return yaw;
+	}
+
+	public void setYaw(double yaw) {
+		this.yaw = yaw;
+	}
+
+	public double getRoll() {
+		return roll;
+	}
+
+	public void setRoll(double roll) {
+		this.roll = roll;
+	}
+
+	/**
+	 * Remove/refactor
+	 * @param engine
+	 * @return
+	 */
+	@Deprecated
 	public static double getDefaultTwoPi(UnrealEngine engine){
 		if(engine == UnrealEngine.UE4){
 			return 360d;
 		} else {
 			return 65536d;
 		}
+	}
+
+	/**
+	 * Switch to UE123 range to UE4 range or vice-versa
+	 * @param isUE4Space
+	 */
+	public void switchSpace(boolean isUE4Space) {
+
+		// switch to UE123 space
+		if (isUE4Space && !this.isUE4Space) {
+			pitch *= 360d / 65536d;
+			yaw *= 360d / 65536d;
+			roll *= 360d / 65536d;
+		}
+		// switch to UE4 space
+		else if (!isUE4Space && this.isUE4Space) {
+			pitch *= 65536d / 360d;
+			yaw *= 65536d / 360d;
+			roll *= 65536d / 360d;
+
+			// in UE4 space the original "X" axis is rotated by 180°
+		}
+
+		this.isUE4Space = isUE4Space;
+	}
+
+	public boolean isUE4Space() {
+		return isUE4Space;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Rotator rotator = (Rotator) o;
+		return isUE4Space == rotator.isUE4Space && Double.compare(rotator.pitch, pitch) == 0 && Double.compare(rotator.yaw, yaw) == 0 && Double.compare(rotator.roll, roll) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(isUE4Space, pitch, yaw, roll);
+	}
+
+	@Override
+	public String toString() {
+		return "(Pitch=" + pitch + ",Yaw=" + yaw + ",Roll=" + roll + ")";
+	}
+
+	public String toUEString(){
+		return "(X=" + pitch + ",Y=" + roll + ",Z=" + yaw + ")";
 	}
 }
--- src/test/java/org/xtx/ut4converter/tools/GeometryTest.java
+++ src/test/java/org/xtx/ut4converter/tools/GeometryTest.java
@@ -2,6 +2,8 @@ package org.xtx.ut4converter.tools;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.xtx.ut4converter.geom.Rotator;
+import org.xtx.ut4converter.t3d.T3DUtils;
 
 import javax.vecmath.Vector3d;
 
@@ -14,4 +16,45 @@ public class GeometryTest {
         Assert.assertEquals(new Vector3d(360d, 90d, 0d), Geometry.UE123ToUE4Rotation(ut99Rotation));
     }
 
+
+    @Test
+    public void testGetRotator(){
+
+        // testing parser
+        final Rotator r1 = T3DUtils.parseRotator("BaseRot=(Pitch=32768,Yaw=-16384,Roll=65536)");
+        Assert.assertEquals(32768, r1.getPitch(), 0.01d);
+        Assert.assertEquals(-16384, r1.getYaw(), 0.01d);
+        Assert.assertEquals(65536, r1.getRoll(), 0.01d);
+        // test default space is UE123 space (not UE4 space)
+        Assert.assertFalse(r1.isUE4Space());
+
+        // test switching from UE123 space to UE4 space (meaning changing range and rotator value)
+        r1.switchSpace(true);
+        Assert.assertEquals(180, r1.getPitch(), 0.01d);
+        Assert.assertEquals(-90, r1.getYaw(), 0.01d);
+        Assert.assertEquals(360, r1.getRoll(), 0.01d);
+        Assert.assertTrue(r1.isUE4Space());
+
+        final Rotator r2 = T3DUtils.parseRotator("BaseRot=(Yaw=-16384,Roll=65536)");
+        Assert.assertEquals(0, r2.getPitch(), 0.01d);
+        Assert.assertEquals(-16384, r2.getYaw(), 0.01d);
+        Assert.assertEquals(65536, r2.getRoll(), 0.01d);
+
+        final Rotator r3 = T3DUtils.parseRotator("BaseRot=(Pitch=32768,Yaw=-16384)");
+        Assert.assertEquals(32768, r3.getPitch(), 0.01d);
+        Assert.assertEquals(-16384, r3.getYaw(), 0.01d);
+        Assert.assertEquals(0, r3.getRoll(), 0.01d);
+
+        final Rotator r4 = T3DUtils.parseRotator("BaseRot=(Pitch=32768,Roll=65536)");
+        Assert.assertEquals(32768, r4.getPitch(), 0.01d);
+        Assert.assertEquals(0, r4.getYaw(), 0.01d);
+        Assert.assertEquals(65536, r4.getRoll(), 0.01d);
+
+        // some real test from IsvKran32 Unreal 1 map
+        final Rotator r = T3DUtils.parseRotator("=(Roll=-19456)");
+        System.out.println(r);
+        r.switchSpace(true);
+        System.out.println(r);
+        System.out.println(r.toUEString());
+    }
 }
