b26734734939134482f94cd56b5d800e4cf57402
xtremexp <unrealxtx@gmail.com>
2023-01-16 14:56:25 +0100

    config: allow using ucc.exe rather than umodel.exe ui: removed texture extractor choice in conv settings

--- src/main/java/org/xtx/ut4converter/MapConverter.java
+++ src/main/java/org/xtx/ut4converter/MapConverter.java
@@ -207,11 +207,6 @@ public class MapConverter extends Task<T3DLevelConvertor> {
 	private boolean noUi;
 
 
-	/**
-	 * Prefered texture extractor for non Unreal 2 game
-	 * If null will use the default one (umodel)
-	 */
-	private Class<? extends UTPackageExtractor> preferedTextureExtractorClass;
 
 	/**
 	 * Global logger
@@ -1507,15 +1502,6 @@ public class MapConverter extends Task<T3DLevelConvertor> {
 
 	public void setUseUbClasses(boolean useUbClasses) {
 		this.useUbClasses = useUbClasses;
-	}
-
-
-	public void setPreferedTextureExtractorClass(Class<? extends UTPackageExtractor> preferedTextureExtractorClass) {
-		this.preferedTextureExtractorClass = preferedTextureExtractorClass;
-	}
-
-	public Class<? extends UTPackageExtractor> getPreferedTextureExtractorClass() {
-		return preferedTextureExtractorClass;
 	}
 
 	public void setNoUi(boolean noUi) {
