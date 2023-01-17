cd78a2e468d0f6092dc0ee3dd1ab6df5d7d964f6
xtremexp <unrealxtx@gmail.com>
2023-01-17 09:45:07 +0100

    ui: added export package panel v2

--- src/main/java/org/xtx/ut4converter/tools/SystemUtil.java
+++
@@ -1,9 +0,0 @@
-package org.xtx.ut4converter.tools;
-
-
-public class SystemUtil {
-
-    public static boolean is32BitOS(){
-        return "i386".equals(System.getProperties().getProperty("os.arch"));
-    }
-}
